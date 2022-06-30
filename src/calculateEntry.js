a6bf5018f017212ec2bfe576e0297d7e37538897
Bruno de Oliveira <historia.brunooliveira1993@gmail.com>
2022-06-30 14:48:43 -0300

    exercicio 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((element) => element.age < 18);
+  const adult = entrants.filter((element) => element.age >= 18 && element.age < 50);
+  const senior = entrants.filter((element) => element.age >= 50);
+  const obj = {
+    child: child.length,
+    adult: adult.length,
+    senior: senior.length,
+  };
+  return obj;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!Array.isArray(entrants)) {
+    return 0;
+  }
+  const peeoples = countEntrants(entrants);
+  const childPay = peeoples.child * 20.99;
+  const adultPay = peeoples.adult * 49.99;
+  const seniorPày = peeoples.senior * 24.99;
+  return childPay + adultPay + seniorPày;
 }
+
+console.log(calculateEntry());
 
 module.exports = { calculateEntry, countEntrants };
