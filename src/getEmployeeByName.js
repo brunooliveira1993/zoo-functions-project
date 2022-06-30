0845e685e4488c292cd513a6d098b9e88171ddbf
Bruno de Oliveira <historia.brunooliveira1993@gmail.com>
2022-06-29 21:24:01 -0300

    correção ecercicio 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  if (employeeName === undefined) return [];
+  if (employeeName === undefined) return {};
   const result = data.employees.filter((element) =>
     element.firstName === employeeName || element.lastName === employeeName);
   return result[0];
